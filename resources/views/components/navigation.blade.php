eddb0541a00b32a8414e66dc51bad0344003ea6a
Nouzi <tomikitd@gmail.com>
2023-04-29 18:25:13 +0200

    pridany navigation.link komponent

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -5,20 +5,24 @@
                 <div class="flex-shrink-0">
                     {{--TODO pridať komponent na logo--}}
                     {{--Logo--}}
-                    <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
+                    <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                         alt="Your Company">
                 </div>
                 <div class="hidden md:block w-full">
                     <div class="ml-10 flex items-baseline space-x-4">
                         {{--TODO pridať komponent na link--}}
-                        @can('admin')
-                            <a href="#" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Admin Panel</a>
-                        @endcan
+
+                        <a href="#" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+                           aria-current="page">Profil</a>
+                        <x-navigation.link href="/profil" name="Profil" />
                         {{--Ukáže členov týmu--}}
-                        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Tím</a>
+                        <a href="#"
+                           class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Tím</a>
                         {{--Ukáže Projekty na ktorých pracuje--}}
-                        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projekty</a>
-                        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Kalendár</a>
-                        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Záznamy</a>
+                        <a href="#"
+                           class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projekty</a>
+                        <a href="#"
+                           class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Záznamy</a>
                         <div class="w-full flex justify-end">
                             <form class="text-white" action="/logout" method="POST">
                                 @csrf
---
+++ resources/views/components/navigation/link.blade.php
@@ -0,0 +1,7 @@
+@props(['name'])
+<a
+   class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+   aria-current="page"
+>
+    {{$name}}
+</a>
