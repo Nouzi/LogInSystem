cbfcace4cdcbc508265f7fa1668abbfc4e28de10
Nouzi <tomikitd@gmail.com>
2024-01-30 19:11:25 +0100

    pridanie nového labelu

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -10,8 +10,9 @@
             @csrf
             <div>
                 <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Meno</label>
+                <label for="surname" class="block text-sm font-medium leading-6 text-gray-900">Surname</label>
                 <div class="mt-2">
-                    <input id="name" name="name" type="text" autocomplete="name" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    <input id="name" name="name" type="text"  autocomplete="name" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                 </div>
             </div>
 
@@ -44,4 +45,3 @@
 </div>
 
 </x-layout>
-
